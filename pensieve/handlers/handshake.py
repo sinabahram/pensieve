73769d3e6ae7027dd9e246b32055819d23de0fe4
Sina Bahram <sina@sinabahram.com>
2014-03-01 11:21:35 -0500

    removed debug printline

--- pensieve/handlers/handshake.py
+++ pensieve/handlers/handshake.py
@@ -4,7 +4,6 @@ from helpers import ok, checkValue, MessageError
 
 class Handshake(HandlerInterface):
     def handle(self, header, data):
-        print "Handshake got called"
         try:
             return checkValue(header, 'value', 'ping', ok('handshake', 'pong'))
         except MessageError as e:
